813ab0f8b52b33672bff0e68f18fb0b19edfe21a
adalcar <tancrede.erulin@gmail.com>
2015-03-26 00:08:09 +0100

    update

--- TP C# 10/tiny42sh/tiny42sh/Execution.cs
+++ TP C# 10/tiny42sh/tiny42sh/Execution.cs
@@ -26,6 +26,7 @@ namespace tiny42sh
             switch (first)
             {
                 case Keyword.cat:
+                    k = execute_cat(cmd);
                     break;
                 case Keyword.cd:
                     k = execute_cd(cmd);
@@ -43,14 +44,14 @@ namespace tiny42sh
                 case Keyword.pwd:
                     break;
                 case Keyword.rm:
-                    if (k != 0)
-                        Console.WriteLine("rm: {0}: {1}", cmd[1], errors[k]);
+                    k = execute_rm(cmd);
                     break;
                 case Keyword.rmdir:
                     if (k != 0)
                         Console.WriteLine("rmdir: {0}: {1}", cmd[1], errors[k]);
                     break;
                 case Keyword.touch:
+                    k = execute_touch(cmd);
                     break;
                 case Keyword.unknown:
                     Console.WriteLine("Unknown command: " + cmd[0]);
@@ -60,6 +61,11 @@ namespace tiny42sh
                 case Keyword.crash:
                     break;
             }
+            if(k != 0)
+                if (k == 4)
+                    Console.Write(cmd[0] + ": " + errors[k]);
+                else
+                    Console.WriteLine("rm: {0}: {1}", cmd[1], errors[k]);
             return 0;
         }
 
@@ -146,10 +152,58 @@ namespace tiny42sh
                 {
                     if (File.Exists(cmd[i]))
                     {
-                        File.ReadAllBytes()
+                        FileStream f = new FileStream(cmd[i], FileMode.Open);
+                        byte[] b = new byte[f.Length];
+                        f.Read(b, 0, (int)f.Length);
+                        f.Close();
+                        foreach(byte bt in b)
+                            Console.Write((char)bt);
+                        Console.WriteLine();
                     }
+                    else
+                        if (Directory.Exists(cmd[i]))
+                            Console.WriteLine("cat: {0}: " + errors[3], cmd[i]);
+                        else
+                            Console.WriteLine("cat: {0}: " + errors[1], cmd[i]);
+                    
                 }
+            return 0;
 
+        }
+
+        private static int execute_touch(string[] cmd)
+        {
+            if (cmd.Length == 0)
+                return 4;
+            else
+                for (int i = 1; i < cmd.Length; i++)
+                {
+                    if (File.Exists(cmd[i]))
+                        File.SetLastAccessTime(cmd[i], DateTime.Now);
+                    else
+
+                        if (Directory.Exists(cmd[i]))
+                            Directory.SetLastAccessTime(cmd[i], DateTime.Now);
+                        else
+                            File.Create(cmd[i]);
+                }
+            return 0;
+        }
+
+        private static int execute_rm(string[] cmd)
+        {
+            if (cmd.Length != 2)
+                return 4;
+            else
+                if (File.Exists(cmd[1]))
+                    File.Delete(cmd[1]);
+                else
+                    if (Directory.Exists(cmd[1]))
+                        return 3;
+                    else
+                        return 1;
+            return 0;
+            
         }
         #endregion
     }
