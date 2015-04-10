a8517ee7228585f98910aa922b92b8773726257d
adalcar <tancrede.erulin@gmail.com>
2015-04-10 10:14:07 +0200

    up

--- TP C# 11/erulin_t/Sudoku/Sudoku/Program.cs
+++ TP C# 11/erulin_t/Sudoku/Sudoku/Program.cs
@@ -10,6 +10,14 @@ namespace Sudoku
     {
         static void Main(string[] args)
         {
+            int a = -25;
+            int b = -24;
+            int c = -350;
+            int d = -2;
+            bool res = Stuff.TimeAfterTime(ref d, ref b, ref a, ref c);
+
+            Console.WriteLine(res + "\ndays: " + d + "\nhours: " + b + "\nmins: " + a + "\nsecs: " + c);
+            Console.Read();
         }
     }
 }
--- TP C# 11/erulin_t/Sudoku/Sudoku/Stuff.cs
+++ TP C# 11/erulin_t/Sudoku/Sudoku/Stuff.cs
@@ -16,13 +16,27 @@ namespace Sudoku
                 int c = sec % 60;
                 if (c < 0)
                     c +=60;
+
                 mins += (sec - c) / 60;
                 sec = c;
             }
             if (mins > 59 || mins < 0)
             {
-
+                int c = mins % 60;
+                if (c < 0)
+                    c += 60;
+                hours += (mins - c) / 60;
+                mins = c;
             }
+            if (hours > 23 || hours < 0)
+            {
+                int c = hours % 24;
+                if (c < 0)
+                    c += 24;
+                days += (hours - c) / 24;
+                hours = c;
+            }
+            return (days > 0);
         }
     }
 }
